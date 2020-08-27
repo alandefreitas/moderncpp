3260b9f80f6eeca89ab47c51d421a859fe0ccb29
alandefreitas <alandefreitas@gmail.com>
2020-08-27 02:33:39 -0300

    SDL + ImGui examples

--- snippets/gui/imgui/imgui_hello.cpp
+++ snippets/gui/imgui/imgui_hello.cpp
@@ -177,8 +177,7 @@ int main(int, char**)
         }
 
         // 3. Show another simple window.
-        if (show_another_window)
-        {
+        if (show_another_window) {
             ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
             ImGui::Text("Hello from another window!");
             if (ImGui::Button("Close Me"))
