3cf048f8b8676c782354e22317cf8f740e7c46ac
alandefreitas <alandefreitas@gmail.com>
2021-02-05 21:11:48 -0300

    Fix CRTP warnings

--- snippets/paradigms/CRTP.cpp
+++ snippets/paradigms/CRTP.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
-#include <numeric>
-#include <string>
-#include <unordered_map>
 
 template <typename derived> class shape {
   public:
-    derived clone() { return static_cast<derived>(*this); };
+    virtual derived clone() { return static_cast<derived>(*this); };
 };
 
 class square : public shape<square> {
   public:
-    square clone() { return *this; }
+    square() = default;
+    explicit square(const shape &){};
+    // CRTP: Clone can return a square even though it's derived from shape
+    square clone() override { return *this; }
 };
 
 int main() {
     square a;
     square b = a.clone();
+
     std::cout << "The square has been cloned as a real square" << std::endl;
     std::cout << "&a: " << &a << std::endl;
     std::cout << "&b: " << &b << std::endl;
