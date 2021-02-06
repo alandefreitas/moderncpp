5b3bdb0025f11edf488f7ff97b8228127f6193db
alandefreitas <alandefreitas@gmail.com>
2021-02-05 21:09:49 -0300

    Add unsigned char type

--- snippets/data_types/data_types.cpp
+++ snippets/data_types/data_types.cpp
@@ -1,15 +1,23 @@
+#include <bitset>
 #include <iostream>
 
 int main() {
     // Basic types
     bool a = true;
     std::cout << "a: " << a << std::endl;
+    std::cout << "sizeof(a): " << sizeof(a) << std::endl;
+
     int b = 25;
     std::cout << "b: " << b << std::endl;
+    std::cout << "sizeof(b): " << sizeof(b) << std::endl;
+
     double c = 1.34;
     std::cout << "c: " << c << std::endl;
+    std::cout << "sizeof(c): " << sizeof(c) << std::endl;
+
     char d = 'g';
     std::cout << "d: " << d << std::endl;
+    std::cout << "sizeof(d): " << sizeof(d) << std::endl;
 
     // Integer precision
     long g = 25;
@@ -26,6 +34,10 @@ int main() {
     std::cout << "k: " << k << std::endl;
     long double l = 987312.325;
     std::cout << "l: " << l << std::endl;
+
+    // Char
+    unsigned char m = 'c';
+    std::cout << "m: " << m << std::endl;
 
     return 0;
 }
