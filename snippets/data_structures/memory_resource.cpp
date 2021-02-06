36e96f4f7e7bed646bbe6bb281befc6d6d3d6922
alandefreitas <alandefreitas@gmail.com>
2021-02-05 21:09:13 -0300

    Add unsynchronized_pool_resource

--- snippets/data_structures/memory_resource.cpp
+++ snippets/data_structures/memory_resource.cpp
@@ -1,33 +1,24 @@
 #include <iostream>
+#include <memory_resource>
 #include <vector>
-#include <map>
-#include <set>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <memory_resource.hpp>
 
 int main() {
-    using namespace std;
-
-    // Create buffer
-    char buffer[64] = {};
+    char buffer[64] = {}; // a small buffer on the stack
     std::fill_n(std::begin(buffer), std::size(buffer) - 1, '_');
     std::cout << buffer << '\n';
-
-    // Create pool
-    feroldi::pmr::monotonic_buffer_resource pool{
-            std::data(buffer), std::size(buffer)
-    };
-
-    // Create vector
-    std::vector<char, feroldi::pmr::polymorphic_allocator<char>> vec{&pool};
+    std::pmr::monotonic_buffer_resource pool{std::data(buffer),
+                                             std::size(buffer)};
+    std::pmr::vector<char> vec{&pool};
     for (char ch = 'a'; ch <= 'z'; ++ch) {
         vec.push_back(ch);
     }
-
     std::cout << buffer << '\n';
 
-
-    return 0;
+    std::pmr::unsynchronized_pool_resource pool2;
+    std::pmr::vector<char> vec2{&pool2};
+    for (char ch = 'a'; ch <= 'z'; ++ch) {
+        vec2.push_back(ch);
+    }
+    std::copy(vec2.begin(), vec2.end(),
+              std::ostream_iterator<char>(std::cout, " "));
 }
