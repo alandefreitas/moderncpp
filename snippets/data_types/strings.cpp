c123853114ea4290aecc405799605567664bc62a
alandefreitas <alandefreitas@gmail.com>
2021-02-07 18:02:25 -0300

    Simplify string example

--- snippets/data_types/strings.cpp
+++ snippets/data_types/strings.cpp
@@ -6,33 +6,42 @@ using namespace std;
 
 int main() {
     // Strings are dynamic arrays of chars
-    string p = "avocado";
-    for (size_t i = 0; i < p.size(); ++i) {
-        cout << "p[" << i << "]: " << p[i] << endl;
+    string str = "avocado";
+
+    // Print the whole string
+    cout << str << '\n';
+
+    // Print the string chars
+    for (size_t i = 0; i < str.size(); ++i) {
+        cout << "str[" << i << "]: " << str[i] << '\n';
     }
 
-    // Look for letter
-    if (p.size() >= 10) {
-        cout << "p[10]: " << p[10] << endl;
+    // Access index
+    if (str.size() >= 10) {
+        cout << "str[10]: " << str[10] << '\n';
     } else {
-        cout << "p[10]: (não existe)" << endl;
+        cout << "str[10]: (does not exist)" << '\n';
     }
 
     // Container functions
-    cout << "p.size(): " << p.size() << endl;
-    cout << "p.empty(): " << p.empty() << endl;
-    cout << "p.front(): " << p.front() << endl;
-    cout << "p.back(): " << p.back() << endl;
+    cout << "str.size(): " << str.size() << '\n';
+    cout << "str.empty(): " << str.empty() << '\n';
+    cout << "str.front(): " << str.front() << '\n';
+    cout << "str.back(): " << str.back() << '\n';
 
     // Contiguous addresses (like any array)
-    cout << "&p: " << &p << endl;
-    cout << "p.data(): " << p.data() << endl;
+    cout << "&str: " << &str << '\n';
+    cout << "str.data(): " << str.data() << '\n';
 
-    // The address of a char prints what is in that position
-    cout << "&p[0]: " << &p[0] << endl;
-    cout << "&p[1]: " << &p[1] << endl;
-    cout << "&p[2]: " << &p[2] << endl;
-    cout << "&p[9]: " << &p[9] << endl;
+    // The subscript operator accesses a single char
+    cout << "str[0]: " << str[0] << '\n';
+    cout << "str[1]: " << str[1] << '\n';
+    cout << "str[2]: " << str[2] << '\n';
+
+    // The address of a char prints from that position
+    cout << "&str[0]: " << &str[0] << '\n';
+    cout << "&str[1]: " << &str[1] << '\n';
+    cout << "&str[2]: " << &str[2] << '\n';
 
     return 0;
 }
