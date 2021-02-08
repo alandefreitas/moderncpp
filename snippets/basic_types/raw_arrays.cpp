9e053900a1254fa9eb0e1bfbfeaf92756ca2382c
alandefreitas <alandefreitas@gmail.com>
2021-02-08 14:03:49 -0300

    Differentiate explicit/implicit size in raw array examples

--- snippets/basic_types/raw_arrays.cpp
+++ snippets/basic_types/raw_arrays.cpp
@@ -7,9 +7,17 @@ using namespace std;
 int main() {
     constexpr int n = 3;
 
-    // Raw array
-    // Examples only: don't use raw arrays!
-    int temp[n] = {1, 2, 3};
+    // Raw arrays
+    // - Examples only: don't use raw arrays!
+
+    // Explicit size
+    int a[n];
+    a[0] = 1;
+    a[1] = 2;
+    a[2] = 3;
+
+    // Implicit size
+    int temp[] = {1, 2, 3};
     for (size_t i = 0; i < n; ++i) {
         cout << "temp[" << i << "]: " << temp[i] << endl;
     }
@@ -17,8 +25,14 @@ int main() {
     // Array is converted to its initial pointer
     cout << "temp: " << temp << endl;
     cout << "&temp: " << &temp << endl;
+
+    cout << "temp[0]: " << temp[0] << endl;
     cout << "&temp[0]: " << &temp[0] << endl;
+
+    cout << "temp[1]: " << temp[1] << endl;
     cout << "&temp[1]: " << &temp[1] << endl;
+
+    cout << "temp[2]: " << temp[2] << endl;
     cout << "&temp[2]: " << &temp[2] << endl;
 
     return 0;
