b648719dc19462f2727fad86ecd07d920910a45e
alandefreitas <alandefreitas@gmail.com>
2021-02-08 14:04:44 -0300

    Add snippet handling error in user input

--- snippets/input_output/cin.cpp
+++ snippets/input_output/cin.cpp
@@ -4,9 +4,23 @@
 using namespace std;
 
 int main() {
-    int age{60};
+    // User input
+    int age;
     std::cout << "How old are you? ";
     std::cin >> age;
+    if (age >= 18) {
+        std::cout << "Welcome!\n";
+    } else {
+        std::cout << "Sorry.\n";
+        return 1;
+    }
+
+    // User input (Handling error)
+    std::cout << "How old are you again? ";
+    if (!(std::cin >> age)) {
+        std::cout << "There is an error in your input\n";
+        return 1;
+    }
     if (age >= 18) {
         std::cout << "Welcome!\n";
     } else {
