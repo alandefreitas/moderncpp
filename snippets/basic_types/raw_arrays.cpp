ee2fb32a8f6044011f12833d1fba9c4aa41b5684
alandefreitas <alandefreitas@gmail.com>
2021-02-12 14:01:46 -0300

    Add array address snippet

--- snippets/basic_types/raw_arrays.cpp
+++ snippets/basic_types/raw_arrays.cpp
@@ -18,8 +18,20 @@ int main() {
 
     cout << "a: " << a << '\n';
 
+    // Accessing elements
+    cout << "a[0]: " << a[0] << '\n';
+    cout << "a[1]: " << a[1] << '\n';
+    cout << "a[2]: " << a[2] << '\n';
+
+    // Accessing element address
+    cout << "&a[0]: " << &a[0] << '\n';
+    cout << "&a[1]: " << &a[1] << '\n';
+    cout << "&a[2]: " << &a[2] << '\n';
+
     // Implicit size
     int b[] = {1, 2, 3};
+
+    // Accessing elements
     for (size_t i = 0; i < n; ++i) {
         cout << "b[" << i << "]: " << b[i] << endl;
     }
@@ -36,6 +48,12 @@ int main() {
 
     cout << "b[2]: " << b[2] << endl;
     cout << "&b[2]: " << &b[2] << endl;
+
+    // Raw strings are just arrays of chars
+    char c[] = { 'a', 'b', 't', '\0' };
+
+    // Raw strings are streamed until '\0' is found
+    cout << "c: " << c << endl;
 
     return 0;
 }
