517460c15867adb1eb2f61564d4e95b0edf4e7a3
alandefreitas <alandefreitas@gmail.com>
2021-02-12 14:03:15 -0300

    Add loop + conditional snippet

--- snippets/control_flow/loops.cpp
+++ snippets/control_flow/loops.cpp
@@ -1,64 +1,106 @@
 #include <array>
 #include <iostream>
 #include <map>
-#include <random>
-#include <string>
 
 using namespace std;
 
 int main() {
-    // Classic loop
-    for (size_t i = 0; i < 10; ++i) {
-        cout << i << endl;
+    // Basic loop
+    for (int i = 0; i < 10; ++i) {
+        cout << "Hello world!" << '\n';
     }
 
-    // Changing update statement
-    int b = 2;
-    int e = 1024;
-    for (int i = b; i <= e; i *= b) {
-        cout << b << endl;
+    // Changing the number of repetitions
+    cout << "How many hello worlds? ";
+    int z;
+    cin >> z;
+    for (int i = 0; i < z; ++i) {
+        cout << "Hello world" << '\n';
     }
 
-    // While loop
+    // Similar tasks according to the counter
+    for (int i = 0; i < 10; ++i) {
+        cout << "Hello world " << i << "!\n";
+    }
+
+    // Changing external variables
+    int total = 0;
+    for (int i = 0; i < 10; ++i) {
+        total += i;
+    }
+    cout << "0+1+2+3+4+...+9: " << total << '\n';
+
+    // Loop + conditional
+    total = 0;
+    for (int i = 0; i < 50; ++i) {
+        const bool is_multiple_of_three = i % 3 == 0;
+        if (is_multiple_of_three) {
+            total += i;
+        }
+    }
+    cout << "0+3+6+9+12+...+48: " << total << '\n';
+
+    // Changing initialization
+    // 5/3 + 7/4 + 9/5 + 11/6 + ...
+    float series = 0.;
+    for (int i = 3.f; i < 50; ++i) {
+        const auto i_float = static_cast<float>(i);
+        series += (i_float * 2 - 1) / i_float;
+    }
+    cout << "5/3+7/4+9/5+...+97/49: " << series << '\n';
+
+    // Changing update statement to +=
+    total = 0;
+    for (int i = 7; i < 500; i += 7) {
+        total += i;
+    }
+    cout << "7+14+21+...+497: " << total << '\n';
+
+    // Changing update statement to *=
+    for (int i = 2; i <= 1024; i *= 2) {
+        cout << i << '\n';
+    }
+
+    // Do-While loop
     int option = 0;
     do {
         ++option;
     } while (option < 4);
 
-    // Index based loop
+    // Index-based loop for containers with subscript operator[]
     constexpr int n = 100;
     array<double, n> prices{};
     for (size_t i = 0; i < prices.size(); ++i) {
         prices[i] = 4 + 0.1 * i;
     }
 
-    // Iterator based loop
-    for (double & price : prices) {
-        cout << "Item price: " << price << endl;
+    // Range-based loop for containers
+    for (double& price : prices) {
+        cout << "Item price: " << price << '\n';
     }
 
-    // Range based loop
+    // Range-based loop with constant references
     double sum = 0.;
     for (const auto &price : prices) {
         sum += price;
     }
-    cout << "Average price: " << sum / n << endl;
+    cout << "Average price: " << sum / n << '\n';
 
-    // Range based loop with value pairs
+    // Range-based loop with value pairs
     map<int, int> t;
     t[7] = 2;
     t[8] = 4;
     t[4] = 6;
     for (const auto &[key, value] : t) {
-        cout << key << ": " << value << endl;
+        cout << key << ": " << value << '\n';
     }
 
-    // Iterating 2-dimensional arrays
+    // Create multidimensional array
     constexpr int n1 = 100;
     constexpr int n2 = 3;
     array<array<double, n2>, n1> grades{};
 
-    // Index based loop
+    // Iterating 2-dimensional arrays with index-based loops
     for (int i = 0; i < n1; ++i) {
         for (int j = 0; j < n2; ++j) {
             grades[i][j] = 6. + i * (2. / n1) + j * (2. / n2);
@@ -71,7 +113,7 @@ int main() {
         for (const auto &col : row) {
             row_sum += col;
         }
-        cout << "Average student grade: " << row_sum / grades.size() << endl;
+        cout << "Average student grade: " << row_sum / grades.size() << '\n';
     }
 
     // Columns in the external for
@@ -81,7 +123,7 @@ int main() {
             col_sum += grades[i][j];
         }
         cout << "Average score on test " << j + 1 << ": " << col_sum / n1
-             << endl;
+             << '\n';
     }
 
     return 0;
